7476ee50d3ba88441c7b8ea3daa14c4356e4c935
etilelab <taegyeong7202@gmail.com>
2022-04-29 22:49:39 +0900

    one cycle

--- main.py
+++ main.py
@@ -1,57 +1,66 @@
-import selenium
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 import time
 import random
+import urllib
 
-# gender : male, female
+# Gender : male, female
 gender = "male"
 
-# hair : black, brown
+# Hair : black, brown
 hair = "black"
 
-# emotion :  Disgusted : 역겨운, Frightened : 겁먹은,
+# Emotion :  Disgusted : 역겨운, Frightened : 겁먹은,
 emotion_dict = {'disgust':'emotiondisgust','angry':'emotionneangry'}
 emotion = emotion_dict['disgust']
 
-# age : child (10-20), adult (20-50), older (50-)
-# If you select an option, that option is chosen by a random number within the range.
-age = "adult"
-if age == "child":
-    random.randrange(1, 7)
-elif age == "adult":
-    random.randrange(1, 7)
-elif age == "older":
-    random.randrange(1, 7)
+# Age
+# website standard age is 19.  Result age is 19 +- age_number
+age_number = random.randrange(-4, 25)
 
-# get url
+# Download image count
+count = 0
+
+# ===================================================
+# Get url
 URL = 'https://generated.photos/face-generator/6268f6b0f44734000f062f19'
 driver = webdriver.Chrome(executable_path='chromedriver')
 driver.get(url=URL)
 
-time.sleep(5)  # wait 5 seconds
+time.sleep(3)  # wait 3 seconds
 
-# gender click
+# Gender click
 if gender == "male":
     driver.find_element_by_id("sexmale").click()
 else:
     driver.find_element_by_id("sexfemale").click()
 
-# hair click
+# Hair click
 if hair == "black":
     driver.find_element_by_id("hair#404040").click()
 elif hair == "brown":
     driver.find_element_by_id("hair#6C4F3D").click()
 
-# emotion click
+# Emotion click
 driver.find_element_by_id(emotion).click()
 
-
+# Age range
 slider = driver.find_element_by_xpath('//*[@id="ageSlider"]/input')
-for i in range(10):
-  slider.send_keys(Keys.RIGHT)
+if age_number > 0:
+    for i in range(age_number):
+      slider.send_keys(Keys.RIGHT)
+else:
+    for i in range(age_number):
+      slider.send_keys(Keys.LEFT)
+
+# Update face image
+driver.find_element_by_xpath('//*[@id="__layout"]/div/div[1]/aside/div[3]/div[2]/button').click()
+time.sleep(7)
+
+face_image = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[1]/main/div[2]/div[1]/img').get_attribute('src')
+urllib.request.urlretrieve(face_image, "FACE_{gender}_{hair}_{emotion}_{count}.jpg".format(gender, hair, emotion, count))
+count = count + 1
+
+# Generate new image
+driver.find_element_by_xpath('//*[@id="__layout"]/div/div[1]/aside/div[3]/div[3]/button').click()
